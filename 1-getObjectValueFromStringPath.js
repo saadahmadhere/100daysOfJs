b15404a1e3629a7b69694b3aa7b0af87fd6f9fdd
saadahmadhere <52596800+saadahmadhere@users.noreply.github.com>
2023-06-07 12:05:38 +0530

    [Day-1]: get object value from String Path

--- 1-getObjectValueFromStringPath.js
+++ 1-getObjectValueFromStringPath.js
@@ -1,1 +1,33 @@
-console.log('heeloo');
+const obj = { a: { b: { c: [1, 2, 3] } } };
+
+// write a function get which can do the following:
+
+// console.log(get(obj, 'a.b.c'))
+// console.log(get(obj, 'a.b.c.0'))
+// console.log(get(obj, 'a.b.c[1]'))
+// console.log(get(obj, ['a', 'b', 'c', '2']))
+// console.log(get(obj, 'a.b.c[3]'))
+// console.log(get(obj, 'a.c'))
+
+function get(obj, path) {
+	// check if path is an empty string or it is an empty array.
+	if (!path || path.length === 0) return undefined;
+
+	const excludeChars = ['[', ']', '.'];
+	const keys = [];
+
+	for (let i = 0; i < path.length; i++) {
+		if (!excludeChars.includes(path[i])) keys.push(path[i]);
+	}
+
+	// let res = keys.reduce((obj, key) => obj[key], obj);
+
+	// return res;
+	let res;
+	for (let ch of keys) {
+		res = res ? res[ch] : obj[ch];
+		console.log(res);
+	}
+
+	return res;
+}
